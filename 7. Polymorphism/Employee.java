6849e1dcf81563b51afb833ba65c54291f36a055
Daoud-Hussain <daoudhussain302@gmail.com>
2022-04-17 21:34:27 -0700

    Adding Polymorphism

---
+++ 7. Polymorphism/Employee.java
@@ -0,0 +1,9 @@
+public class Employee {
+	protected String firstName;
+	protected String lastName;
+	protected String ssn;
+
+
+	//Abstract class method
+	protected abstract double earnings();
+}
---
+++ 7. Polymorphism/WeeklyEmployee.java
@@ -0,0 +1,11 @@
+public class WeeklyEmployee extends Employee{
+	private double WeeklySalary;
+
+	//Method to calculate Weekly Salary
+	public double earnings(){
+		return WeeklySalary;
+	}
+	
+
+
+}
