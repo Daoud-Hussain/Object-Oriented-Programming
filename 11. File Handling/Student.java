c08ee8431d998b1ff0bee8aa89969daeacc12775
Daoud-Hussain <daoudhussain302@gmail.com>
2022-05-30 10:28:59 +0500

    Extending classes in file handling

---
+++ 11. File Handling/Student.java
@@ -0,0 +1,60 @@
+public class Student extends Person{
+	private double gpa; 
+	private int semester; 
+	private String section; 
+	private Department typeDepartment;
+
+	public Student(){
+		//Default Argument Constructor
+		Department myDept = new Department();
+	}
+
+	public Student(String name, String phone, String gender, double gpa, int semester, String section, Department myDept){
+		super(name, phone, gender);
+		this.gpa = gpa;
+		this.semester = semester;
+		this.section = section;
+		this.typeDepartment = myDept;
+	}
+
+	//Setters
+	public void setGPA(double gpa){
+		if(gpa != 0)
+			this.gpa = gpa;
+	}
+
+	public void setSemester(int semester){
+		if(semester != 0)
+			this.semester = semester;
+	}
+
+	public void setSection(String section){
+		if(section != "")
+			this.section = section;
+	}
+
+	public void setDepartment(Department deptObj){
+		if(deptObj != null)
+			this.typeDepartment = deptObj;
+	}
+
+
+	//Getters
+	public double getGPA(){
+		return this.gpa;
+	}
+
+	public String getSection(){
+		return this.section;
+	}
+
+	public int getSemester(){
+		return this.semester;
+	}
+
+	public Department getTypeDepartment(){
+		return this.typeDepartment;
+	}
+
+
+}
